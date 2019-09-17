7df1991952d42c210093822064e5d019ec9d95dc
Anton Ziuzikov <zuzikov.anton@gmail.com>
2019-09-17 16:09:56 +0300

    feat: implement interfaces in classes

--- jslab/src/app/card-list/card-list.component.ts
+++ jslab/src/app/card-list/card-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import IBoard from "../models/Board";
+import ICard from "../models/Card";
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.css']
 })
-export class CardListComponent implements OnInit {
+export class CardListComponent implements OnInit, IBoard {
+
+  id: string;
+  name: string;
+  cards: ICard[];
 
   constructor() { }
 
--- jslab/src/app/card/card.component.ts
+++ jslab/src/app/card/card.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import ICard from "../models/Card";
+import IUser from "../models/User";
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, ICard {
+
+  id: string;
+  name: string;
+  description: string;
+  dueDate?: Date| string;
+  assignee?: IUser;
 
   constructor() { }
 
